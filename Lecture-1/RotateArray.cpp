d33c15090da3e81a01217c41914bde08a55e6a70
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-03-24 18:06:25 +0530

    Lecture-1 Arrays

--- Lecture-1/RotateArray.cpp
+++ Lecture-1/RotateArray.cpp
@@ -1,17 +1,39 @@
-// RotateArray
-#include<iostream> 
-#include<vector>
-using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        if( n== 0 || n==1){
+            return;
+        }
+        k = k%n;
 
-void rotate(vector<int>& nums, int k) {
-	
-
-}		
-
-int main(){
-
-
-
-
-	return 0; 
-}
+        int i = 0;
+        int j = n-1;
+        
+        // 1. Reverse the array
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+        
+        // 2. Swap first k elements
+        i = 0;
+        j = k-1;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+        
+        // 3. Swap the last n-k elements
+        i = k;
+        j = n-1;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+    }
+};
